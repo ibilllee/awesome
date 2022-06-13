952eb07b127b5df9c7ce6de441c6353c92927fe1
NightMu <805357409@qq.com>
2022-06-13 16:06:25 +0800

    feat: 修改类中变量的类型
    	modified:   common/src/main/java/com/scut/common/dto/response/SearchGameListDto.java

--- common/src/main/java/com/scut/common/dto/response/SearchGameListDto.java
+++ common/src/main/java/com/scut/common/dto/response/SearchGameListDto.java
@@ -3,7 +3,7 @@ package com.scut.common.dto.response;
 public class SearchGameListDto
 {
     long id; //游戏id
+    long score; // 游戏评分
     String name; //游戏名称
     String classify; //游戏分类
-    long score; // 游戏评分
 }
