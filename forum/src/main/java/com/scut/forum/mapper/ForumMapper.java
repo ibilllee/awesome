0e843fa3eb4ed56b809441b99cfeaea52a136438
李逸轩 <ibilllee@foxmail.com>
2022-06-14 16:16:19 +0800

    feat: 完成Forum/ForumController的所有接口

--- forum/src/main/java/com/scut/forum/mapper/ForumMapper.java
+++ forum/src/main/java/com/scut/forum/mapper/ForumMapper.java
@@ -3,8 +3,11 @@ package com.scut.forum.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.scut.forum.entity.Forum;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultType;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,4 +15,8 @@ import java.util.Map;
 public interface ForumMapper extends BaseMapper<Forum> {
     @Select(" SELECT cover, logo FROM game WHERE id = #{gameId} ")
     Map<Object,String> selectCoverAndLogoByGameId(long gameId);
+
+    @Select({"${sql}"})
+    @ResultType(Forum.class)
+    List<Forum> executeListQuery(@Param("sql") String sql);
 }
---
+++ forum/src/main/java/com/scut/forum/mapper/ForumTagMapper.java
@@ -0,0 +1,9 @@
+package com.scut.forum.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.scut.forum.entity.ForumTag;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ForumTagMapper extends BaseMapper<ForumTag> {
+}
